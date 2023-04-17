1d46f487123c442989367e3597ce98280b7b93d9
rian <rnevcasey@gmail.com>
2023-04-17 10:51:43 -0400

    added navbar and fixed default styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 import RegisterForm from "./components/RegisterForm";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [token, setToken] = useState(null);
 
   return (
     <div className="App">
+      <NavBar />
       <RegisterForm />
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <img src="../../logo.jpg" alt="" className="nav-image" />
+      <button>Login</button>
+    </nav>
+  );
+}
